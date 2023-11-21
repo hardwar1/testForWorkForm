59a2759203ca1d3515f1c857e8b9581590015426
Serg k <xapgbap1@ya.ru>
2023-11-21 10:22:54 +0300

    add react-route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { PopupForm } from './shared/PopupForm'
 // import styles from './app.module.scss'
 
 function App() {
 
   return (
-    <>
-      <PopupForm />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/testForWorkForm/' element={<PopupForm />} />
+        <Route path="*" element={<Navigate to="/testForWorkForm/" replace />} />
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 
 export default App
+
